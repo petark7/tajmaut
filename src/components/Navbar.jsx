c348e4b5b15b6f71709904a8d88e2103412758d4
Petar <kuzmanovski.petar@uklo.edu.mk>
2023-02-10 15:40:08 +0100

    Implement active tab in navbar
    Using react Navlink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,51 @@
 import "./css/Navbar.css"
+import {NavLink} from "react-router-dom"
+
+const activeStyle = {
+    color: "#4F3E94",
+    "font-weight" : "600",
+    "text-decoration": 'none',
+    padding: "10px 20px",
+    background: "#CAC5E7",
+    "border-radius": "10px"
+ }
+
+ const inactiveStyle = {
+    color: "#4F3E94",
+    "text-decoration": 'none',
+    padding: "10px 20px"
+ }
 
 export default function Navbar () {
+
     return (
         <div className="navbar-container">
                 <img className = "logo" src = {require('../data/images/logo.png')}/>
                 <ul className="navbar_links">
-                    <a href ="/">Насловна</a>
-                    <a href ="/schedule">Распоред</a>
-                    <a href ="/venues">Локали</a>
+                    <li>
+                        <NavLink to="/" style={({isActive}) => 
+                                 isActive ? activeStyle : inactiveStyle
+                                 }>
+                                    Home
+                        </NavLink>
+                    </li>
+
+                    <li>
+                        <NavLink to="/schedule" style={({isActive}) => 
+                                 isActive ? activeStyle : inactiveStyle
+                                 }>
+                                    Распоред
+                        </NavLink>
+                    </li>
+
+                    <li>
+                        <NavLink to="/venues" style={({isActive}) => 
+                                 isActive ? activeStyle : inactiveStyle
+                                 }>
+                                    Локали
+                        </NavLink>
+                    </li>
+
                 </ul>
                 <button className="navbar-button_login">Најава</button>
         </div>
--- src/routes/venues.jsx
+++ src/routes/venues.jsx
@@ -1,6 +1,5 @@
 import Navbar from "../components/Navbar"
 
-
 export default function Venues () {
     return (
         <div className="container--home_page">
