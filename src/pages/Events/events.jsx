f73b7bb86eb62fdd8e19666dcab85f4f7d5bed98
Petar <kuzmanovski.petar@uklo.edu.mk>
2023-02-15 21:07:27 +0100

    Implement event_list
    + refactor css

--- src/pages/Events/events.jsx
+++ src/pages/Events/events.jsx
@@ -1,13 +1,26 @@
 import EventCard from "../../components/EventCard/EventCard.jsx";
+import eventData from "../../data/mockdata.json"
+import "./events.css"
 
 export default function Events() {
 
+    const events = eventData.map(event => 
+        {
+            return(<EventCard 
+                key = {event.id}
+                name = {event.name}
+                city = {event.location.city}
+                venue = {event.location.venue}
+                image = {event.poster_image}
+                date = {event.date}
+                />)
+        })
+
     return (
         <div className="container--events">
-              <>
-              <h1>Events</h1>
-              <EventCard/>
-              </>
+              <div className="event-list">
+                    {events}
+              </div>
         </div>
     );
   };
