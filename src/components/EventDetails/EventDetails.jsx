9d3bf4bbe348978f521383e9f4c406116dc9f1e2
petark7 <kuzmanovski.petar@uklo.edu.mk>
2023-02-18 12:52:58 +0100

    Update EventDetails

--- src/components/EventDetails/EventDetails.jsx
+++ src/components/EventDetails/EventDetails.jsx
@@ -6,10 +6,20 @@ export default function EventDetails (props) {
         <div id="myModal" class="modal">
             <div class="modal-content">
                 <span class="close" onClick={props.closeModal}>&times;</span>
-                <p>{props.name}</p>
+                <UnderlinedLabel label="Кога?" value="петок - 23/4/2022"/>
             </div>
         </div>
 
         </>
+    )
+}
+
+function UnderlinedLabel (props) {
+    return (
+        <div className="underlinedLabel">
+            <h3 className="underlinedLabel--title">{props.label}</h3>
+            <div className="underlinedLabel--line"/>
+            <h4 className="underlinedLabel--value">{props.value}</h4>
+        </div>
     )
 }
