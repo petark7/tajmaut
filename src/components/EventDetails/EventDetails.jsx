8f2bd8cae17a19e2a91a9a3756bf06a510d43b84
petark7 <kuzmanovski.petar@uklo.edu.mk>
2023-02-18 18:06:29 +0100

    Update EventDetails
    Further improve alignment
    Add title

--- src/components/EventDetails/EventDetails.jsx
+++ src/components/EventDetails/EventDetails.jsx
@@ -10,6 +10,7 @@ export default function EventDetails (props) {
                     <img className="eventDetailsImg" src={require(`../../img/${props.image}`)}/>
                         <div className="eventDetailsContent">
                             <div className="close-button--container"> 
+                            <h1 className="eventDetails--title">DJ IRIE SCRATCH</h1>
                                 <svg className="close" onClick={props.closeModal} width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M0.82153 0.821407C1.4724 0.170533 2.52768 0.170533 3.17855 0.821407L12 9.6429L20.8215 0.821407C21.4724 0.170533 22.5277 0.170533 23.1786 0.821407C23.8294 1.47228 23.8294 2.52756 23.1786 3.17843L14.3571 11.9999L23.1786 20.8214C23.8294 21.4723 23.8294 22.5276 23.1786 23.1784C22.5277 23.8293 21.4724 23.8293 20.8215 23.1784L12 14.3569L3.17855 23.1784C2.52768 23.8293 1.4724 23.8293 0.82153 23.1784C0.170655 22.5276 0.170655 21.4723 0.82153 20.8214L9.64302 11.9999L0.82153 3.17843C0.170655 2.52756 0.170655 1.47228 0.82153 0.821407Z" fill="black"/>
                                 </svg>
