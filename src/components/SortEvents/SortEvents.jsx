a2e355ea15ed8a0fadf8491c6348e7d4195e541c
petark7 <kuzmanovski.petar@uklo.edu.mk>
2023-02-21 16:54:11 +0100

    Apply Form to DropList

--- src/components/SortEvents/SortEvents.jsx
+++ src/components/SortEvents/SortEvents.jsx
@@ -1,15 +1,34 @@
 import "./SortEvents.css"
 import DatePickerElements from "../DatePicker/DatePicker"
 import cityArray from "../../data/cities.json"
+import { useForm } from "react-hook-form";
+import React from "react";
+
+const cities = cityArray.map ((city) => {
+    return (
+        <option value={city.value}>{city.name}</option>
+    )
+})
 
 export default function SortEvents () {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data));
+      };
+
     return (
         <div className="sortEvents-container">
+             <form onSubmit={handleSubmit(onSubmit)}>
                 <Heading label="Период на прикажување"/>
                 <DatePickerElements/>
                 <Heading label="Локација"/>
-                <CityDropdown/>
-            <button className="sortEvents-btnPrikazhi button">Прикажи</button>
+                <select name="city" id="sortEvents-cityDropdown" {...register('selected_city')}>
+                    {cities}
+                </select>
+                <input type="submit" value = "Сортирај" className="sortEvents-btnPrikazhi button"/>
+             </form>
+
+               
         </div>
     )
 }
@@ -20,20 +39,5 @@ function Heading (props) {
             <h1 className="datePickLabel--labelContent">{props.label}</h1>
             <div className="decorativeLine"/>
         </div>
-    )
-}
-
-function CityDropdown () {
-
-    const cities = cityArray.map ((city) => {
-        return (
-            <option value={city.value}>{city.name}</option>
-        )
-    })
-
-    return (
-        <select name="city" id="sortEvents-cityDropdown">
-            {cities}
-        </select>
     )
 }
