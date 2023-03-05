e2394fdc8d2776a7758ba972e1eb486932112eda
Hristijan Ralevski <kiksa_mk@hotmail.com>
2023-03-05 20:06:06 +0100

    Change's for Pull request

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,53 +3,45 @@ import React, { useState } from "react";
 
 export default function LoginForm({ onSignUpClick }) {
   return (
-    
-      <div className="log-in">
-        <h1>Најава</h1>
+    <div className="log-in">
+      <h1>Најава</h1>
 
-        <div className="alt-login">
-          <div className="google">
-            <h2>Продолжи со google</h2>
-          </div>
+      <div className="alt-login">
+        <div className="google">
+          <h2>Продолжи со google</h2>
         </div>
-
-        <h2 className="or">
-          <span>или</span>
-        </h2>
-
-        <form method="post">
-          <div className="user">
-            <input type="text" required />
-            <span></span>
-            <label>Корисничко име/E-пошта</label>
-          </div>
-
-          <div class="user">
-            <input type="password" required />
-            <span></span>
-            <label>Лозинка</label>
-          </div>
-
-          <input
-            className="remember"
-            type="checkbox"
-            id="remember"
-            name="vehicle1"
-            value="Bike"
-          />
-
-          <label for="remember">Запамти ме</label>
-
-          <div className="pass">Заборавена лозинка?</div>
-          <input type="submit" value="Најави се" />
-          <div className="signup">
-            Немаш профил ?
-            <a href="#" onClick={onSignUpClick}>
-              Регистрирај се
-            </a>
-          </div>
-        </form>
       </div>
 
+      <h2 className="or">
+        <span>или</span>
+      </h2>
+
+      <form>
+        <div className="user">
+          <input type="text" required />
+          <span></span>
+          <label>Корисничко име/E-пошта</label>
+        </div>
+
+        <div class="user">
+          <input type="password" required />
+          <span></span>
+          <label>Лозинка</label>
+        </div>
+
+        <input className="remember" type="checkbox" id="remember" />
+
+        <label for="remember">Запамти ме</label>
+
+        <div className="pass">Заборавена лозинка?</div>
+        <input type="submit" value="Најави се" />
+        <div className="signup">
+          Немаш профил ?
+          <a href="#" onClick={onSignUpClick}>
+            Регистрирај се
+          </a>
+        </div>
+      </form>
+    </div>
   );
 }
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,83 +1,70 @@
 import "./Register.css";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function RegisterForm({ onLoginClick }) {
+  const [isShown, setIsSHown] = useState(false);
+  const togglePassword = () => {
+    setIsSHown((isShown) => !isShown);
+  };
 
-    const [isShown, setIsSHown] = useState(false);
-    const togglePassword = () => {
-        setIsSHown((isShown) => !isShown);
-      };
+  return (
+    <div className="register-in">
+      <h1>Регистрација</h1>
 
-      
-    return (
-        <div className="register-in">
-        <h1>Регистрација</h1>
+      <form>
+        <section>
+          <div className="user">
+            <input type="text" required />
 
-            <form method="post">
+            <label>Име</label>
+          </div>
 
-                <section>
-                <div className="user">
-                    <input  type="text" required />
-                    <span></span>
-                    <label>Име</label>
-                </div>
+          <div class="user">
+            <input type="text" required />
+            <span></span>
+            <label>Презиме</label>
+          </div>
+        </section>
 
-                <div class="user">
-                    <input type="text" required />
-                    <span></span>
-                    <label>Презиме</label>
-                </div>
-                </section>
+        <section>
+          <div className="user">
+            <input type="text" required />
+            <span></span>
+            <label>E-пошта</label>
+          </div>
+        </section>
 
-                <section>
-                <div className="user">
-                    <input  type="text" required />
-                    <span></span>
-                    <label>E-пошта</label>
-                </div>
+        <section>
+          <div className="user">
+            <input type={isShown ? "text" : "password"} required />
+            <span></span>
+            <label>Лозинка</label>
+          </div>
 
-                </section>
+          <div class="user">
+            <input type={isShown ? "text" : "password"} required />
+            <span></span>
+            <label>Потврди</label>
+          </div>
+        </section>
+        <input
+          id="checkbox"
+          type="checkbox"
+          checked={isShown}
+          onChange={togglePassword}
+        />
 
-                <section>
-                <div className="user">
-                    <input  
-                    type={isShown ? "text" : "password"} 
-                    required />
-                    <span></span>
-                    <label>Лозинка</label>
-                </div>
+        <label for="checkbox">Прикажи лозинка</label>
 
-                <div class="user">
-                    <input 
-                     type={isShown ? "text" : "password"}
-                     required 
-                    />
-                    <span></span>
-                    <label>Потврди</label>
-                </div>
-                </section>
-                <input 
-                id="checkbox" 
-                type="checkbox" 
-                checked={isShown}
-                onChange={togglePassword}
-                
-                 />
-
-                <label for="checkbox">Прикажи лозинка</label>
-
-                <input type="submit" value="Регистрирај се" />
-                <div className="signup">
-                    Веќе си зачленет?<a href="#" onClick={onLoginClick}> Најави се</a>
-                </div>
-            </form>
-
-            
+        <input type="submit" value="Регистрирај се" />
+        <div className="register">
+          Веќе си зачленет?
+          <a href="#" onClick={onLoginClick}>
+            {" "}
+            Најави се
+          </a>
         </div>
-
-        
-    )
-
-
-
+      </form>
+    </div>
+  );
 }
