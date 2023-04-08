e19ad3f9bca19e3906b3525eb3b57b8252654891
Petar <kuzmanovski.petar@uklo.edu.mk>
2023-04-09 00:09:36 +0200

    Fix modals auto close bug
    ForgotPassword + Register

--- src/components/ForgotPass/Password.jsx
+++ src/components/ForgotPass/Password.jsx
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 
 export default function PasswordForm({ onLoginClick, onCloseClick }) {
   return (
-    <div className="overlay" onClick={onCloseClick}>
+   <>
+     <div className="overlay" onClick={onCloseClick}/>
       <div className="forgot custom-modal">
         <h1>Промена на Лозинка <p className="X" onClick={onCloseClick} >X</p> </h1>
         <p>
@@ -27,6 +28,6 @@ export default function PasswordForm({ onLoginClick, onCloseClick }) {
           </div>
         </form>
       </div>
-    </div>
+   </>
   );
 }
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -8,7 +8,8 @@ export default function RegisterForm({ onLoginClick, onCloseClick }) {
   };
 
   return (
-    <div className="overlay" onClick={onCloseClick}>
+   <>
+    <div className="overlay" onClick={onCloseClick}/>
       <div className="register-in custom-modal">
         <h1>
           Регистрација{" "}
@@ -76,6 +77,6 @@ export default function RegisterForm({ onLoginClick, onCloseClick }) {
           </div>
         </form>
       </div>
-    </div>
+   </>
   );
 }
