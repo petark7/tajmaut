0e567c0c1cbb12d15d0735b9d416cc6bec215689
Petar <kuzmanovski.petar@uklo.edu.mk>
2023-04-10 01:53:38 +0200

    Format Data

--- src/components/HomeSlider/HomeSlider.jsx
+++ src/components/HomeSlider/HomeSlider.jsx
@@ -1,10 +1,12 @@
 import React, { Component, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 import Slider from "react-slick";
 import axios from "axios"
 import "./HomeSlider.css";
 
 export default function SimpleSlider (props) {
 
+  const navigate = useNavigate();
   const numberOfEvents = 4;
   const [eventData, setEventData] = useState([{
     eventId: "",
@@ -17,13 +19,59 @@ export default function SimpleSlider (props) {
     venueCity: "",
 }])
 
-  const handleClick = () => {
+  const getDateTimeDay = (dateTime) => {
+    const UTCDate = new Date (Date.parse(dateTime));
+    const CESTDate = new Date(UTCDate.getTime() + (2 * 60 * 60 * 1000));
+    const formattedDate = `${CESTDate.getDate().toString().padStart(2, '0')}/${(CESTDate.getMonth() + 1).toString().padStart(2, '0')}/${CESTDate.getFullYear()}`;
     
+    const hours =  CESTDate.getHours() < 10 ? `0${CESTDate.getHours()}` : CESTDate.getHours()
+    const minutes = CESTDate.getMinutes() < 10 ? `0${CESTDate.getMinutes()}` : CESTDate.getMinutes();
+    const time = `${hours}:${minutes}`
+    let day = UTCDate.getDay();
+    switch (day)
+    {
+        case 0:
+        day = "Недела";
+        break;
+
+        case 1:
+        day = "Понеделник";
+        break;
+
+        case 2:
+        day = "Вторник";
+        break;
+
+        case 3:
+        day = "Среда";
+        break;
+
+        case 4:
+        day = "Четврток";
+        break;
+
+        case 5:
+        day = "Петок";
+        break;
+
+        case 6:
+        day = "Сабота";
+        break;
+    }
+
+    return {
+      date: formattedDate,
+      "time": time,
+      day: day,
+    }
+  }
+
+  const handleClick = () => {
   }
   
   let events = [];
-
     events = eventData.map((event) => {
+      const {date, time, day} = getDateTimeDay(event.dateTime)
       return (
         <div className="slider" key={event.eventId}>
           <div className="slider--content">
@@ -35,12 +83,12 @@ export default function SimpleSlider (props) {
             <div className="slider-content">
               <h1 className="slider--eventTitle">{event.name}</h1>
               <h2 className="slider--dateLocation">
-                {event.dateTime} {event.venueCity} - {" "}
+                {date} {event.venueCity} - {" "}
                 {event.venueName}
               </h2>
-              <h2 className="slider--time">18:00 часот</h2>
+              <h2 className="slider--time">{time} часот</h2>
               <div className="buttonContainer">
-                <button onClick={handleClick} className="homeSlider-reserveBtn">
+                <button onClick={() => navigate(`/make-reservation/${event.eventId}`)} className="homeSlider-reserveBtn">
                   Резервирај
                 </button>
               </div>
